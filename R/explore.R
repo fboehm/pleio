5f91ce9294419d17912af127b252ef19efce5b83
fboehm <frederick.boehm@gmail.com>
2016-08-17 10:51:00 -0500

    incorporated Brian's suggestions

--- R/explore.R
+++ R/explore.R
@@ -4,8 +4,7 @@ genomat <- probs$probs$`19`
 # Function for input to apply(, MARGIN = 3)
 #ie, how do we treat a matrix of genotype probabilities from a single locus?
 hk_residuals <- function(pheno, genomat){
-  xmat <- cbind(1, genomat[, 2:8])
-  lm1 <- lm(pheno ~ xmat)
+  lm1 <- lm(pheno ~ genomat[, 2:8])
   return(residuals(lm1))
 }
 
@@ -29,19 +28,20 @@ yy <- cbind(y1, y2)
 
 res_mat <- apply(FUN = function(x)hk_residuals(pheno = yy, genomat = x), X = probs$probs$`19`, MARGIN = 3)
 
-out <- list()
-for (k in 1:3311){
-  out[[k]] <- hk_residuals(yy, probs$probs$`19`[, , k])
+nmar <- dim(probs$probs$`19`)[3]
+out <- array(NA, c(nrow(yy),ncol(yy),nmar))
+for (k in 1:nmar){
+  out[, , k] <- hk_residuals(yy, probs$probs$`19`[, , k])
 }
 
 
 
 # make a matrix of determinants of cross prods
 M <- dim(probs$probs$`19`)[3]
-deter_mat <- matrix(nrow = M, ncol = M)
-for (i in 1:M){
+deter_mat <- matrix(nrow = nmar, ncol = nmar)
+for (i in 1:nmar){
   for (j in 1:i){
-    deter_mat[i, j] <- det(t(out[[i]]) %*% out[[j]])
+    deter_mat[i, j] <- det(t(out[, , i]) %*% out[, , j])
   }
 }
 
@@ -50,22 +50,22 @@ range(deter_mat, na.rm = TRUE)
 ###############
 ###############
 
-out2a <- list()
-out2b <- list()
-for (k in 1:3311){
-  out2a[[k]] <- hk_residuals(y1, probs$probs$`19`[, , k])
-  out2b[[k]] <- hk_residuals(y2, probs$probs$`19`[, , k])
+out2 <- array(NA, c(nrow(yy), ncol(yy), nmar))
+for (k in 1:nmar){
+  out2[, 1, k] <- hk_residuals(y1, probs$probs$`19`[, , k])
+  out2[, 2, k] <- hk_residuals(y2, probs$probs$`19`[, , k])
 }
 
-deter_mat2 <- matrix(nrow = M, ncol = M)
-out2 <- list()
-for (i in 1:M){
-  out2[[i]] <- cbind(c(out2a[[i]], rep(0, 294)), c(rep(0, 294), out2b[[i]]))
+deter_mat2 <- matrix(nrow = nmar, ncol = nmar)
+for (i in 1:nmar){
   for (j in 1:i){
-    deter_mat2[i, j] <- det(t(out2[[i]]) %*% out2[[j]])
+    deter_mat2[i, j] <- det(t(out2[, , i]) %*% out2[, , j])
   }
 }
 
-reshape2::melt(deter_mat2)
-deter_mat2 - deter_mat -> diff
-range(diff, na.rm = TRUE)
+
+
+
+#reshape2::melt(deter_mat2)
+#deter_mat2 - deter_mat -> diff
+#range(diff, na.rm = TRUE)
