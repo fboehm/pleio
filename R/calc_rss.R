b6c4b9301b32ead8bed921c4d212855c07197217
fboehm <frederick.boehm@gmail.com>
2016-09-09 10:40:34 -0500

    renamed calc_dets function to calc_rss

--- R/calc_rss.R
+++ R/calc_rss.R
@@ -1,10 +1,10 @@
-#' Calculate determinant of 'squared residuals' at two loci for two (ordered) traits
+#' Calculate determinant of 'squared residuals' (ie, RSS) at two loci for two (ordered) traits
 #'
 #' @param yy a n x 2 matrix containing two phenotypes
 #' @param mat1 a matrix of genotype (founder) probabilities with first column being all 1's
 #' @param mat2 a matrix of genotype (founder) probabilities with first column being all 1's
 #' @export
-calc_dets <- function(yy, mat1, mat2){
+calc_rss <- function(yy, mat1, mat2){
   res1 <- hk_residuals(pheno = yy[, 1], mat1)
   res2 <- hk_residuals(pheno = yy[, 2], mat2)
   res_mat <- cbind(res1, res2)
