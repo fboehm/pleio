fa5b7ba4bb219b9073e1adb085a55d8bc94e784c
fboehm <frederick.boehm@gmail.com>
2016-09-09 10:31:06 -0500

    added Mb units to plot

--- R/calc_dets.R
+++ R/calc_dets.R
@@ -1,8 +1,8 @@
 #' Calculate determinant of 'squared residuals' at two loci for two (ordered) traits
 #'
 #' @param yy a n x 2 matrix containing two phenotypes
-#' @param mat1 a matrix of genotype (founder) probabilities
-#' @param mat2 a matrix of genotype (founder) probabilities
+#' @param mat1 a matrix of genotype (founder) probabilities with first column being all 1's
+#' @param mat2 a matrix of genotype (founder) probabilities with first column being all 1's
 #' @export
 calc_dets <- function(yy, mat1, mat2){
   res1 <- hk_residuals(pheno = yy[, 1], mat1)
