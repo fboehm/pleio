6bc419142b2c6356bcb92e2566f5864854beb99b
fjboehm <fjboehm@ad.wisc.edu@dyn-128-104-118-219.biochem.wisc.edu>
2016-09-15 13:22:46 -0500

    updated R/ directory & removed calc_rss() function.

---
+++ R/calc_profile_lods.R
@@ -0,0 +1,16 @@
+#' Calculate profile LODs for two traits
+#'
+#' @param log10det_rss matrix containing log10determinants of RSS
+#' @export
+
+calc_profile_lods <- function(log10det_rss){
+  log10det_rss0 <- diag(log10det_rss)
+  log10det_rss_row_mins <- apply(FUN = min, X = log10det_rss, MARGIN = 1)
+  # find min value per column of logrss
+  log10det_rss_col_mins <- apply(FUN = min, X = log10det_rss, MARGIN = 2)
+  p1 <- calc_profile_lod(log10det_rss_mins = log10det_rss_row_mins, log10det_rss0 = log10det_rss0, n_mouse = dim(probs$probs$`19`)[1])
+  p2 <- calc_profile_lod(log10det_rss_mins = log10det_rss_col_mins, log10det_rss0 = log10det_rss0, n_mouse = dim(probs$probs$`19`)[1])
+  return(list(p1, p2))
+}
+
+
--- R/profile_plot.R
+++ R/profile_plot.R
@@ -1,14 +1,15 @@
 #' Plot profile LR statistics for two traits
 #'
-#' @param profile1 profile lod statistic for trait one
-#' @param profile2 profile lod statistic for trait two
+#' @param profiles list of profile lod statistics for two traits
 #' @param marker_positions vector of marker positions (should have length equal to that of profile1 & profile2)
 #' @param col1 color of trait 1 plot
 #' @param col2 color of trait 2 plot
 #' @export
 
 
-profile_plot <- function(profile1, profile2, marker_positions, col1 = "red", col2 = "blue"){
+profile_plot <- function(profiles, marker_positions, col1 = "red", col2 = "blue"){
+  profile1 <- profiles[[1]]
+  profile2 <- profiles[[2]]
   df <- data.frame(profile1, profile2, marker_positions)
   ggplot2::ggplot(df)  +
     ggplot2::geom_line(colour = col1, ggplot2::aes(x = marker_positions, y = profile1)) +
