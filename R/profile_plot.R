a1c622a28b7725db6bbe52ae30b4cc750f6c8801
fboehm <frederick.boehm@gmail.com>
2016-09-19 09:00:38 -0500

    added col3 as input parameter & updated doc.

--- R/profile_plot.R
+++ R/profile_plot.R
@@ -4,10 +4,11 @@
 #' @param marker_positions vector of marker positions (should have length equal to that of profile1 & profile2)
 #' @param col1 color of trait 1 plot
 #' @param col2 color of trait 2 plot
+#' @param col3 color of pleiotropy-based trace plot
 #' @export
 
 
-profile_plot <- function(profiles, marker_positions, col1 = "red", col2 = "blue"){
+profile_plot <- function(profiles, marker_positions, col1 = "red", col2 = "blue", col3 = "black"){
   profile1 <- profiles[[1]]
   profile2 <- profiles[[2]]
   joint <- profiles[[3]]
@@ -15,6 +16,6 @@ profile_plot <- function(profiles, marker_positions, col1 = "red", col2 = "blue"
   ggplot2::ggplot(df)  +
     ggplot2::geom_line(colour = col1, ggplot2::aes(x = marker_positions, y = profile1)) +
     ggplot2::geom_line(colour = col2, ggplot2::aes(x = marker_positions, y = profile2)) +
-    ggplot2::geom_line(colour = "black", ggplot2::aes(x = marker_positions, y = joint)) +
+    ggplot2::geom_line(colour = col3, ggplot2::aes(x = marker_positions, y = joint)) +
     ggplot2::labs(x = "Marker position (Mb)", y = "LOD")
 }
