49943799604db91fbcee2c4eb415532602567acf
fboehm <frederick.boehm@gmail.com>
2016-09-25 10:23:05 -0500

    added option for true profile calcs in calc_lod()

--- R/calc_lod.R
+++ R/calc_lod.R
@@ -2,19 +2,30 @@
 #'
 #' @param data tibble containing log10determinants of RSS
 #' @param n_mouse sample size
+#' @param true_profile_lik indicator (defaults to TRUE) of whether to calculate true profile lod, ie, by maximizing over the nuisance parameter $\lambda_i$
 #' @export
 
-calc_lod <- function(data, n_mouse){
+calc_lod <- function(data, n_mouse, true_profile_lik = TRUE){
   # define log10detrss0
   log10detrss0 <- data %>%
     dplyr::filter(marker1 == marker2)
-  argmin <- data %>%
-    dplyr::filter(log10detrss == min(log10detrss))
-  profile1 <- data %>%
-    dplyr::filter(marker1 == argmin$marker1)
-  profile2 <- data %>%
-    dplyr::filter(marker2 == argmin$marker2)
-  return(tibble::tibble(lod1 = - n_mouse * profile1$log10detrss / 2,
-                        lod2 = - n_mouse * profile2$log10detrss / 2,
-                        joint = - n_mouse * log10detrss0$log10detrss / 2))
+  if (!true_profile_lik){
+    argmin <- data %>%
+      dplyr::filter(log10detrss == min(log10detrss))
+    profile1 <- data %>%
+      dplyr::filter(marker1 == argmin$marker1)
+    profile2 <- data %>%
+      dplyr::filter(marker2 == argmin$marker2)
+  }
+  if (true_profile_lik){
+    profile1 <- data %>%
+      dplyr::group_by(marker2) %>%
+      min(log10detrss)
+    profile2 <- data %>%
+      dplyr::group_by(marker1) %>%
+      min(log10detrss)
+  }
+    return(tibble::tibble(lod1 = - n_mouse * (profile1$log10detrss - min(log10detrss0$log10detrss)) / 2,
+                        lod2 = - n_mouse * (profile2$log10detrss - min(log10detrss0$log10detrss))/ 2,
+                        joint = - n_mouse * (log10detrss0$log10detrss - - min(log10detrss0$log10detrss)) / 2))
 }
