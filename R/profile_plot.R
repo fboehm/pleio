3e29e19cc0baf57eb3977e5a3522b9a13bc237b3
fboehm <frederick.boehm@gmail.com>
2016-09-27 15:44:34 -0500

    moved colour argument within call to aesthetic to get legend to show

--- R/profile_plot.R
+++ R/profile_plot.R
@@ -14,8 +14,9 @@ profile_plot <- function(profiles, marker_positions, col1 = "red", col2 = "blue"
   joint <- profiles[, 3]
   df <- data.frame(profile1, profile2, joint, marker_positions)
   ggplot2::ggplot(df)  +
-    ggplot2::geom_line(colour = col1, ggplot2::aes(x = marker_positions, y = profile1)) +
-    ggplot2::geom_line(colour = col2, ggplot2::aes(x = marker_positions, y = profile2)) +
-    ggplot2::geom_line(colour = col3, ggplot2::aes(x = marker_positions, y = joint)) +
-    ggplot2::labs(x = "Marker position (Mb)", y = "LOD")
+    ggplot2::geom_line(ggplot2::aes(colour = col1, x = marker_positions, y = profile1)) +
+    ggplot2::geom_line(ggplot2::aes(colour = col2, x = marker_positions, y = profile2)) +
+    ggplot2::geom_line(ggplot2::aes(colour = col3, x = marker_positions, y = joint)) +
+    ggplot2::labs(x = "Marker position (Mb)", y = "LOD") +
+    ggplot2::guide_legend()
 }
