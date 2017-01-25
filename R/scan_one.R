e75bda074747b2a3931b4ab81f2f07c08e59cc49
Frederick Boehm <frederick.boehm@gmail.com>
2017-01-25 14:48:56 -0600

    added functions for one-dimensional HK scan using only genotypes as covariates

---
+++ R/scan_one.R
@@ -0,0 +1,26 @@
+#' Calculate RSS for a single locus and a single trait
+#' 
+#' @param y a vector of phenotype values, length n
+#' @param genomat a single genotype matrix, n x 8
+#' @export
+calc_rss_one <- function(y, genomat){
+  res <- residuals(lm(y ~ cbind(1, genomat[, -1])))
+  return(sum(res^2))
+}
+
+
+#' One-dimensional scan over a region of interest using only intercept and genotypes
+#' 
+#' @param y a vector of n phenotype values 
+#' @param genoarray a three-dimensional array of genotypes, like `probs$probs$`1``
+#' @param start index for start of scan
+#' @param stop index for stop of scan
+#' @export
+scan_one <- function(y, genoarray, start , stop){
+  n <- length(y)
+  rss <- vector(length = stop - start + 1)
+  for (i in start:stop){
+    rss[i - start + 1] <- calc_rss_one(y, genoarray[ , , i])
+  }
+  return(rss / n)
+}
