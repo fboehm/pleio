03e49ac9e39073d04025dd29a302a13becaed63b
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-07 13:18:44 -0500

    fixed typos & updated doc for calc_ll_bvlmm()

--- R/calc_ll_bvlmm.R
+++ R/calc_ll_bvlmm.R
@@ -1,13 +1,13 @@
-#' Calculate log-likelihood for a LMM
+#' Calculate log-likelihood for a bivariate LMM
 #' 
-#' @param yvec a vector of phenotype values
-#' @param Xmat a design matrix
-#' @param betahat estimate of beta
-#' @param var a covariance matrix
+#' @param yvec a vector of phenotype values, after removing NA values
+#' @param Xmat a design matrix with needed number of rows
+#' @param betahat estimate of beta; a vector of length 16
+#' @param var a covariance matrix with needed dimensions
 #' @export
 
 calc_ll_bvlmm <- function(yvec, Xmat, betahat, var){
-  length(y) -> n
+  length(yvec) -> n
   c_term <- - n * log(2 * pi) / 2
   logdet_term <- - log(det(var)) / 2
   res_term <- - t(yvec - Xmat %*% betahat) %*% solve(var) %*% (yvec - Xmat %*% betahat) / 2
