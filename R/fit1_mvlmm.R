68a773e672dad288be740eb97c3f0779a4f52c3f
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-07 09:27:02 -0500

    corrected variance structure to 5 components

--- R/fit1_mvlmm.R
+++ R/fit1_mvlmm.R
@@ -18,6 +18,7 @@ fit1_bvlmm <- function(Y, X1, X2, Kmat, reml = TRUE){
   In <- diag(1, n)
   V1 <- matrix(c(1, 0, 0, 0), nrow = 2)
   V2 <- matrix(c(0, 0, 0, 1), nrow = 2)
+  V12 <- matrix(c(1, 0, 0, 1), nrow = 2)
   V3 <- matrix(c(0, 1, 1, 0), nrow = 2)
   if (reml == TRUE) {
     kernel <- NULL
@@ -25,22 +26,22 @@ fit1_bvlmm <- function(Y, X1, X2, Kmat, reml = TRUE){
   else {
     kernel <- 0
   }
-  K1 <- V1 %x% Kmat
-  K2 <- V2 %x% Kmat
+  K12 <- V12 %x% Kmat
   K3 <- V3 %x% Kmat
   I1 <- V1 %x% In
   I2 <- V2 %x% In
   I3 <- V3 %x% In
   
   out <- regress::regress(as.matrix(as.vector(Y)) ~ Xmat - 1,
-                 ~ K1 + K2 + K3 + I1 + I2 + I3,
-                 #~ diag(1, 2) %x% Kmat + V1 %x% In + V2 %x% In + V3 %x% In,
+   #              ~ K1 + K2 + K3 + I1 + I2 + I3,
+                 ~ K12 + K3 + I1 + I2 + I3,
+                #~ diag(1, 2) %x% Kmat + V1 %x% In + V2 %x% In + V3 %x% In,
                  identity = FALSE,
                  #kernel = NULL, 
-                 pos = c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE),
+                 pos = c(TRUE, FALSE, TRUE, TRUE, FALSE),
                  #pos = c(TRUE, TRUE, TRUE, FALSE)
                  verbose = 10,
-                 start = c(1, 1, 0, 1, 1, 0)
+                 start = c(1, 0, 1, 1, 0)
                  )
   return(out)
 }
