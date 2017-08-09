9fc6a969870ea8574f7f9a562ba6944670d735fb
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-09 12:47:46 -0500

    fixed variance structure

--- R/fit1_mvlmm.R
+++ R/fit1_mvlmm.R
@@ -20,20 +20,21 @@ fit1_bvlmm <- function(Y, X1, X2, Kmat){
   V2 <- matrix(c(0, 0, 0, 1), nrow = 2)
   V12 <- matrix(c(1, 0, 0, 1), nrow = 2)
   V3 <- matrix(c(0, 1, 1, 0), nrow = 2)
-  
-  K12 <- V12 %x% Kmat
+  # define matrices 
+  K1 <- V1 %x% Kmat
+  K2 <- V2 %x% Kmat
   K3 <- V3 %x% Kmat
   I1 <- V1 %x% In
   I2 <- V2 %x% In
   I3 <- V3 %x% In
-  
+  # call regress
   out <- regress::regress(as.matrix(as.vector(Y)) ~ Xmat - 1,
    #              ~ K1 + K2 + K3 + I1 + I2 + I3,
-                 ~ K12 + K3 + I1 + I2 + I3,
+                 ~ K1 + K2 + K3 + I1 + I2 + I3,
                  identity = FALSE,
-                 pos = c(TRUE, FALSE, TRUE, TRUE, FALSE),
+                 pos = c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE),
                  verbose = 10,
-                 start = c(1, 0, 1, 1, 0)
+                 start = c(1, 1, 0, 1, 1, 0)
                  )
   return(out)
 }
