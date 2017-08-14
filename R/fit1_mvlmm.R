fecb10e1689731696fbdf1cf6eb44e1197aa6177
Frederick Boehm <frederick.boehm@gmail.com>
2017-08-14 12:13:00 -0500

    updated fit1_mvlmm.R

--- R/fit1_mvlmm.R
+++ R/fit1_mvlmm.R
@@ -5,9 +5,11 @@
 #' @param X2 genotype probabilities for second locus, one row per subject
 #' @param Kmat a kinship matrix
 #' @param tol value of tol to be passed to regress()
+#' @param maxcyc value of maxcyc passed to regress()
+#' @param verbose value of verbose passed to regress()
 #' @export
 
-fit1_bvlmm <- function(Y, X1, X2, Kmat, tol = 0.0000001){
+fit1_bvlmm <- function(Y, X1, X2, Kmat, tol = 0.0000001, maxcyc = 100, verbose = 10){
   # assemble Xmat design matrix
   n <- nrow(Y)
   n_founders <- ncol(X1)
@@ -18,7 +20,6 @@ fit1_bvlmm <- function(Y, X1, X2, Kmat, tol = 0.0000001){
   In <- diag(1, n)
   V1 <- matrix(c(1, 0, 0, 0), nrow = 2)
   V2 <- matrix(c(0, 0, 0, 1), nrow = 2)
-  V12 <- matrix(c(1, 0, 0, 1), nrow = 2)
   V3 <- matrix(c(0, 1, 1, 0), nrow = 2)
   # define matrices 
   K1 <- V1 %x% Kmat
@@ -33,9 +34,11 @@ fit1_bvlmm <- function(Y, X1, X2, Kmat, tol = 0.0000001){
                  ~ K1 + K2 + K3 + I1 + I2 + I3,
                  identity = FALSE,
                  pos = c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE),
-                 verbose = 10,
-                 start = c(1, 1, 0, 1, 1, 0), 
-                 tol = tol
+                 #pos = FALSE, 
+                 verbose = verbose,
+                 start = c(0.1, 0.1, 0, 1, 1, 0), 
+                 tol = tol, 
+                 maxcyc = maxcyc
                  )
   return(out)
 }
