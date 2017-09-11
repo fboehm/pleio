61334d260d1a21c7957ee30fc51b597b7d4fea56
Frederick Boehm <frederick.boehm@gmail.com>
2017-09-11 14:23:54 -0500

    fixed calc_lod by adding .data$ to variable names. Also, added rlang R package to imports

--- R/calc_lod.R
+++ R/calc_lod.R
@@ -3,14 +3,15 @@
 #' @param data tibble containing log10determinants of RSS
 #' @param n_mouse sample size
 #' @export
+#' @importFrom rlang .data
 
 calc_lod <- function(data, n_mouse){
   # define log10detrss0
-  log10detrss0 <- dplyr::filter(data, marker1 == marker2)
-  pre1 <- dplyr::group_by(data, marker1)
-  profile1 <- dplyr::summarise(pre1, profile = min(log10detrss))
-  pre2 <- dplyr::group_by(data, marker2)
-  profile2 <- dplyr::summarise(pre2, profile = min(log10detrss))
+  log10detrss0 <- dplyr::filter(data, .data$marker1 == .data$marker2)
+  pre1 <- dplyr::group_by(data, .data$marker1)
+  profile1 <- dplyr::summarise(pre1, profile = min(.data$log10detrss))
+  pre2 <- dplyr::group_by(data, .data$marker2)
+  profile2 <- dplyr::summarise(pre2, profile = min(.data$log10detrss))
   return(tibble::tibble(lod1 = - n_mouse * (profile1$profile - min(log10detrss0$log10detrss)) / 2,
                         lod2 = - n_mouse * (profile2$profile - min(log10detrss0$log10detrss))/ 2,
                         joint = - n_mouse * (log10detrss0$log10detrss - min(log10detrss0$log10detrss)) / 2))
